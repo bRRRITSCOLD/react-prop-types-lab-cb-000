62ff616d33a921a52776b36ffb4608c1ff148858
Blaine Richardson <blainerrichardson@gmail.com>
2017-11-21 21:00:38 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,53 @@
 // Code Product Component Here
+import React from 'react'
+import PropTypes from 'prop-types';
+
+class Product extends React.Component{
+  render(){
+    return (
+      <div>
+        <ul>
+          <li>Name: {this.props.name}</li>
+          <li>Producer: {this.props.producer}</li>
+          <li>Has Watermark: {this.props.hasWatermark}</li>
+          <li>Color: {this.props.color}</li>
+          <li>Weight: {this.props.weight}</li>
+        </ul>
+
+      </div>
+    )
+  }
+}
+/*
+function weightRange(props, propName, componentName){
+  componentName = componentName || 'ANONYMOUS';
+  if (props[propName]) {
+    let value = props[propName];
+    if (value >= 80 && value <= 300 && typeof value === 'number') {
+      return value;
+    } else {
+      return new Error(propName + " in ", + componentName + "is out of weight range.");
+    }
+  }
+}*/
+var weightRange = [];
+for(let i = 80; i <= 300; i++) {
+  weightRange.push(i);
+}
+
+console.log(weightRange);
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white','eggshell-white','salmon']).isRequired,
+  weight: PropTypes.oneOf(weightRange).isRequired
+
+};
+
+export default Product;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom';
 import Product from './components/Product';
 
 ReactDOM.render(
-  <Product
-    name="Dunder Mifflin"
-    producer="PaperCo"
-    color="white"
-    weight={210}
-  />,
-  document.getElementById('global')
+  <Product name="Dunder Mifflin" producer="PaperCo" color="white" weight={210} />,
+  document.getElementById('main')
 );
+
+require('./test/index-test.js'); // Leave this in!
